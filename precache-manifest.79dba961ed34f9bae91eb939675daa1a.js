9ae3d9200821c7ab82e508c3c4cf668e8201a621
Mangesh <mgandhi93@gmail.com>
2020-09-09 13:40:38 -0700

    Updates

---
+++ precache-manifest.79dba961ed34f9bae91eb939675daa1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b29f0f675120d5d44702",
+    "url": "/InstantSearch/static/css/main.e5637243.chunk.css"
+  },
+  {
+    "revision": "b29f0f675120d5d44702",
+    "url": "/InstantSearch/static/js/main.b29f0f67.chunk.js"
+  },
+  {
+    "revision": "253c5dc42a8e42100cce",
+    "url": "/InstantSearch/static/js/1.253c5dc4.chunk.js"
+  },
+  {
+    "revision": "b07c669b66e25a84a36c",
+    "url": "/InstantSearch/static/js/runtime~main.b07c669b.js"
+  },
+  {
+    "revision": "c2bba8a18710f475d27a88b702f05548",
+    "url": "/InstantSearch/index.html"
+  }
+];
